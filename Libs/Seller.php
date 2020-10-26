981eb6b6acb4ce7049f97eeeb244dd972dd098c4
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-10-26 15:09:12 +0800

    投標帳號輪替，可實現換預設帳號，剩下Member

--- Libs/Seller.php
+++ Libs/Seller.php
@@ -1,5 +1,7 @@
 <?php
 namespace BibianBidAccount\Libs;
+
+use mysqli;
 class Seller
 {   
     private $sellerID;//賣家帳號
@@ -10,21 +12,25 @@ class Seller
 
     private $yahooAccount;//當前指派Yahoo帳號
 
-    private $accountList;
+    private $connect;//DB的連接
 
     public function __construct($sellerID)
     {
-        if ($this->isSellerExist($sellerID)){
-            $this->loadInfoFromDB($sellerID);//抓DB裡面的資料來更新這次用的Seller 
+        $this->sellerID = $sellerID;
+        $this->connect = new mysqli('localhost','root','','bid_account');
+        if ($this->isSellerExist($this->sellerID)){
+            $this->loadInfoFromDB($this->sellerID);//抓DB裡面的資料來更新這次用的Seller 
         } else{
-            $this->createSeller($sellerID);
+            $this->createSeller($this->sellerID);
         }
 
     }
 
-    private function _assignAccount()
+    private function assignAccount()
     {
-        $this->yahooAccount = $this->accountList[$this->accountCounter];
+        include "YahooAccount.php";
+        $numOfAccount = count($AccountList);
+        $this->yahooAccount = $AccountList[($this->accountCounter)%$numOfAccount];
         $this->accountCounter += 1 ;
     }
 
@@ -47,10 +53,13 @@ class Seller
         return $this->accountCounter;
     }
 
-    public function isSellerExist(){
-        $take_SellerID = "SELECT * FROM `Seller_list` WHERE `sellerID`= $sellerID";   
-        $result = $connect->query($take_SellerID);
-        if (is_null($result)){
+    public function isSellerExist($sellerID){
+        $take_SellerID = "SELECT * FROM `Seller_list` WHERE `sellerID`= '$sellerID'";
+  
+        $result = $this->connect->query($take_SellerID);
+
+        if (empty($result->num_rows)){
+            echo "該Seller為第一次被投標，等等需指定新帳號";
             return false;
         } else {
             return true;
@@ -60,29 +69,43 @@ class Seller
     private function createSeller($sellerID){
         $this->sellerID = $sellerID;
         //AccountList不應該寫在Class裡面，找時間拿出去
-        $this->accountList  = array(
-            '0' => '帳號0',
-            '1' => '帳號1',
-            '2' => '帳號2',
-            '3' => '帳號3',
-            '4' => '帳號4',
-            '5' => '帳號5',
-        );
-        $this->_assignAccount();
         $this->accountCounter = 0;
+        $this->assignAccount();
+
         $this->product = array();
         $this->saveInfoToDB();
     }
 
     private function saveInfoToDB(){
-
+        $saveSellerToSQL="INSERT INTO 
+        Seller_list(sellerID, YahooAccountNow, AccountCounter) 
+        VALUES ('$this->sellerID', '$this->yahooAccount', '$this->accountCounter')
+        ON DUPLICATE KEY UPDATE YahooAccountNow='$this->yahooAccount', AccountCounter='$this->accountCounter'";
+        $this->connect->query($saveSellerToSQL);
     }
 
     private function loadInfoFromDB($sellerID){
-
+        $take_sellerID = "SELECT * FROM `Seller_list` WHERE `sellerID`= '$sellerID'";   
+        $result = $this->connect->query($take_sellerID);
+        $row = $result -> fetch_array(MYSQLI_BOTH);
+        $this->yahooAccount = $row['YahooAccountNow'];
+        $this->accountCounter = $row['AccountCounter'];
     }
 
     public function returnSellerID(){
         return $this->sellerID;
     }
+
+    public function returnYahooAccount(){
+        
+        return $this->yahooAccount;
+    }
+
+    public function changeToNextAccount()
+    {
+        $this->assignAccount();
+        $this->saveInfoToDB();
+        echo '已經重新指派帳號為:'.$this->yahooAccount.'<br>';
+    }
+
 }
--- main_stepOfBid.php
+++ main_stepOfBid.php
@@ -1,16 +1,29 @@
 <?PHP
-
-$Product_ID = $_POST['Product_ID']; //賣場編號
-$bidPrice = $_POST['bidPrice']; //用戶出價
-$member_ID = 78706;//會員編號
+include 'config.php';
+use BibianBidAccount\Libs\MemberForBid;
+// $productID = $_POST['productID']; //賣場編號
+// $bidPrice = $_POST['bidPrice']; //用戶出價
+$productID = 'c1122334455';
+$sellerID = 'seller_1121';
+$bidPrice = 3260;
+$memberID = 15555;//會員編號
 $bidStatus = 0;//最後出價、立即出價
 
 
-$Member = new MemberForBid($member_ID, $Product_ID);
+$Member = new MemberForBid($memberID, $productID);
+
 $Member->setBidPrice($bidPrice);
 $Member->setBidStatus($bidStatus);//立即或最後出價
-$Member->doBid();
+$Member->setSellerID($sellerID);
 
+// $Member->testSucess = true;//出價成功
+$Member->testSucess = false;//出價成功
+
+// var_dump($Member);
+// exit();
+$Member->doBid($productID, $bidPrice);
+//$Member->echoYahooAccuount();
+//var_dump($Member);
 
 
 
